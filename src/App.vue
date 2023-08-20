d1912b36e0e4c893b574856608df779b25cc66ee
ronmiguelvaldez <ronmiguelvaldez@gmail.com>
2023-08-20 18:49:51 +0800

    Transferred Contents from App.vue to Helloworld.vue

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,15 @@
 <template>
   <div id="app">
-    <p>
-      Number: {{ number }}
-      <!-- ${number}-->
-    </p>
-    <button @click="decrease">decrease</button>
-    <button @click="reset">reset</button>
-    <button @click="increase">increase</button><br />
-    <label> Note: Double Click for Reset</label> <br />
-    <br />
-    <label> Input your name</label>
-    <input type="text" @keydown.enter="showinput" v-model="name" /> <br />
-    <button @click="removetext">Remove Text</button>
-    <p>Hello, {{ showName }}</p>
+    <HelloWorld />
   </div>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
-  components: {},
-  data() {
-    return {
-      number: 0,
-      name: "",
-      showName: "",
-    };
-  },
-  methods: {
-    decrease() {
-      return (this.number -= 1);
-    },
-    reset() {
-      this.number = 0;
-    },
-    increase() {
-      return (this.number += 1);
-    },
-    showinput() {
-      this.showName = this.name;
-    },
-    removetext() {
-      this.showName = "";
-      this.name = "";
-    },
+  components: {
+    HelloWorld
   },
 };
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,50 @@
 <template>
   <div class="hello">
- 
+    <h1>
+      {{ number }}
+    </h1>
+    <button @click="decrease">decrease</button>
+    <button @click="reset">reset</button>
+    <button @click="increase">increase</button><br />
+    <label> Note: Double Click for Reset</label> <br />
+    <br />
+    <label> Input your name</label>
+    <input type="text" @keydown.enter="showinput" v-model="name"/> <br>
+    <button @click="removetext">Remove Text</button>
+    <p>Hello, {{ showName }}</p>
   </div>
 </template>
 
 <script>
+export default {
+  name: 'HelloWorld',
 
+  data() {
+    return {
+      number: 0,
+      name: "",
+      showName: "",
+    };
+  },
+  methods: {
+    decrease() {
+      return (this.number -= 1);
+    },
+    reset() {
+      this.number = 0;
+    },
+    increase() {
+      return (this.number += 1);
+    },
+    showinput() {
+      this.showName = this.name;
+    },
+    removetext() {
+      this.showName = "";
+      this.name = "";
+    },
+  },
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
